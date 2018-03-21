0e213604948cc54a42a31ea08c9bb7359f75de08
Gaston Snaider <gnsnaider@gmail.com>
2018-03-21 12:08:25 -0300

    start ipcsGen process

---
+++ museum/src/ipcsGen.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	printf("Starting creation of ipcs.\n");
+	exit(0);
+}
--- museum/src/main.c
+++ museum/src/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <unistd.h> 
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void create_ipcs() {
+	pid_t ipcs_gen_id = fork();
+	if (ipcs_gen_id == 0) {
+		execl("./ipcsGen", "./ipcsGen", (char*)NULL);
+		printf("Error calling ipcsGen\n");
+		exit(-1);
+	}
+	int status;
+	waitpid(ipcs_gen_id, &status, 0);
+	if (status < 0) {
+		printf("Error executing ipcsGen\n");
+		exit(-1);
+	}
+}
 
 int main(int argc, char* argv[]) {
-	printf("Hello World\n");
+	printf("Starting museum simulation.\n");
+	create_ipcs();
+
 }
