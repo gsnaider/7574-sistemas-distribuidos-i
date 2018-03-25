8da0828aa7f15145665eaf5a156157c9289f5db4
Gaston Snaider <gnsnaider@gmail.com>
2018-03-25 15:28:07 -0300

    Use logger on other files

--- museum/src/entranceDoor.c
+++ museum/src/entranceDoor.c
@@ -2,10 +2,12 @@
 #include <unistd.h>
 #include "message.h"
 #include "include/msg.h"
+#include "include/logger.h"
+
 
 
 int main(int argc, char* argv[]) {
-	printf("Entrance door created with pid %d\n", getpid());
+	safelog("Entrance door created.");
 	
 	int req_queue_id;
     sscanf(argv[1], "%d", &req_queue_id);
@@ -16,11 +18,11 @@ int main(int argc, char* argv[]) {
     int resp_queue = getmsg(resp_queue_id);
 
 	if (req_queue < 0) {
-		printf("Error getting req message queue.\n");
+		safeperror("Error getting req message queue.");
 		exit(-1);
 	}
 	if (resp_queue < 0) {
-		printf("Error getting resp message queue.\n");
+		safeperror("Error getting resp message queue.");
 		exit(-1);
 	}
 
@@ -29,13 +31,13 @@ int main(int argc, char* argv[]) {
 	while (true) {
 		message_t msg;
 		rcvmsg(req_queue, &msg, sizeof(message_t), 0);
-		printf("Processing request...\n");
+		safelog("Processing request...");
 		sleep(1);
 
 		//TODO: Check shm if theres space.
 
 		msg.type = ACCEPT;
-		printf("Finished processing request\n");
+		safelog("Finished processing request");
 		sendmsg(resp_queue, &msg, sizeof(message_t));
 	}
 
--- museum/src/include/msg.h
+++ museum/src/include/msg.h
@@ -13,13 +13,13 @@ int creamsg(int id){
     key_t clave;
     clave = ftok(DIRECTORY, id);
     if (clave < 0) {
-        printf("Error on ftok while creating message queue: %s\n", strerror(errno));
+        safeperror("Error on ftok while creating message queue: %s");
         return -1;
     }
     /* da error si ya existe */
     int msg_id = msgget(clave,  IPC_CREAT | IPC_EXCL | 0660);
     if (msg_id < 0) {
-        printf("Error on msgget while creating message queue: %s\n", strerror(errno));
+        safeperror("Error on msgget while creating message queue: %s");
     }
     return msg_id;
 }
@@ -28,32 +28,32 @@ int getmsg(int id){
     key_t clave;
     clave = ftok(DIRECTORY, id);
     if (clave < 0) {
-        printf("Error on ftok while getting message queue: %s\n", strerror(errno));
+        safeperror("Error on ftok while getting message queue: %s");
         return -1;
     }
     int msg_id = msgget(clave, 0660);
     if (msg_id < 0) {
-        printf("Error on msgget while getting message queue: %s\n", strerror(errno));
+        safeperror("Error on msgget while getting message queue: %s");
     }
     return msg_id;
 }
 
 void sendmsg(int id, const void *msgp, size_t msgsz){
     if(msgsnd(id,msgp,msgsz-sizeof(long),0)==-1){
-        printf("Error sending message over queue: %s\n", strerror(errno));
+        safeperror("Error sending message over queue: %s");
     }
 }
 
 void rcvmsg(int id, void *msgp, size_t msgsz, long type){
     if(msgrcv(id,msgp,msgsz-sizeof(long),type,0)==-1){
-        printf("Error receiving message over queue: %s\n", strerror(errno));
+        safeperror("Error receiving message over queue: %s");
     }
 }
 
 int   delmsg(int id){
     int res = msgctl(id, IPC_RMID, NULL);
     if (res < 0) {
-        printf("Error deleting message queue: %s\n", strerror(errno));
+        safeperror("Error deleting message queue: %s");
     }
     return res;
 }
