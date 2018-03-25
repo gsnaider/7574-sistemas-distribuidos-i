b121767053a3a3a3d67b1f07e555ccf5c1332b06
Gaston Snaider <gnsnaider@gmail.com>
2018-03-25 02:40:40 -0300

    Create exit door

---
+++ museum/src/exitDoor.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hi, I'm an exit door with queue ids %s and %s!\n", argv[1], argv[2]);
+}
--- museum/src/main.c
+++ museum/src/main.c
@@ -24,6 +24,11 @@ void safe_exit(char* exit_msg) {
 int main(int argc, char* argv[]) {
 	printf("Starting museum simulation.\n");
 
+	printf("Creating museum.\n");
+	// init shm with museum cap.
+	printf("Finished creating museum.\n");
+	
+
 	printf("Starting creation of entrance doors.\n");
 	for (int i = 0; i < ENTRANCE_DOORS; i++) {
 		int entrace_req_msg_queue_id = 2 * i;
@@ -51,9 +56,40 @@ int main(int argc, char* argv[]) {
 			safe_exit("ERROR executing entrance door");
 		}
 	}
+	printf("Finished creation of entrance doors.\n");
+	
+	printf("Creating exit door.\n");
+
+	int exit_req_msg_queue_id = 2 * ENTRANCE_DOORS;
+	int exit_resp_msg_queue_id = 2 * ENTRANCE_DOORS + 1;
+
+	if (creamsg(exit_req_msg_queue_id) < 0) {
+		safe_exit("ERROR creating exit req msg queue.");
+	}
+	if (creamsg(exit_resp_msg_queue_id) < 0) {
+		safe_exit("ERROR creating exit resp msg queue.");
+	}
+
+	char req_queue_str[3];
+	snprintf(req_queue_str, 3, "%d", exit_req_msg_queue_id);
+
+	char resp_queue_str[3];
+	snprintf(resp_queue_str, 3, "%d", exit_resp_msg_queue_id);
+
+	pid_t pid = fork();
+	if (pid < 0) {
+		safe_exit("ERROR forking exit door");
+	}
+	if (pid == 0) {
+		execl("./exitDoor", "./exitDoor", req_queue_str, resp_queue_str, (char*)NULL);
+		safe_exit("ERROR executing exit door");
+	}
+
+	printf("Finished creating exit door.\n");
+
 
 	// Wait for all processes to finish.
-	for (int i = 0; i < ENTRANCE_DOORS; i++) {
+	for (int i = 0; i < ENTRANCE_DOORS + 1; i++) {
 		wait((int*) NULL);
 	}
 
