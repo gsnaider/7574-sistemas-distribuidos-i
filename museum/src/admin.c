5f4ac9e0c26a7a7efba43b725c4298b40b081d47
Gaston Snaider <gnsnaider@gmail.com>
2018-03-25 23:44:16 -0300

    Fix admin input

--- museum/src/admin.c
+++ museum/src/admin.c
@@ -3,11 +3,11 @@
 
 
 void open_museum() {
-	safelog("Opening museum\n");
+	safelog("Opening museum");
 }
 
 void close_museum() {
-	safelog("Closing museum\n");
+	safelog("Closing museum");
 }
 
 int main(int argc, char* argv[]) {
@@ -19,8 +19,12 @@ int main(int argc, char* argv[]) {
 		printf("Chose an option:\n");
 		printf("1. Open museum\n");
 		printf("2. Close museum\n");	
-		printf("3. Exit\n");	
-		scanf("%d", &option);
+		printf("3. Exit\n");
+		
+		char* option_str;
+		scanf("%s", option_str);
+		sscanf(option_str, "%d", &option);
+
 		if (option == 1) {
 			open_museum();
 		} else if (option == 2) {
