15342ecc9c5d4a39455a464c14c19a4edb2a0229
Gaston Snaider <gnsnaider@gmail.com>
2018-05-16 13:16:07 -0300

    Attempting to delete from DB
    Cant unsubscribe yet. Get a File not found for the topic file

--- broker/server/db.c
+++ broker/server/db.c
@@ -14,6 +14,7 @@
 #define BASE_DB_DIR "../db/"
 #define USERS_DIR "../db/users/"
 #define TOPICS_DIR "../db/topics/"
+#define TMP_FILE "/tmp/topic_tmp"
 #define INT_MAX_LENGTH 12
 
 #define DB_SEM 11
@@ -86,18 +87,28 @@ static int create_topics_dir_if_not_exists() {
     }
 }
 
-void get_topic_path(char *topic, char *path) {
+static void get_topic_path(char *topic, char *path) {
     strcpy(path, TOPICS_DIR);
     strcat(path, topic);
 }
 
-void get_user_path(int id, char *path) {
+static void get_user_path(int id, char *path) {
     char id_str[INT_MAX_LENGTH];
     snprintf(id_str, INT_MAX_LENGTH, "%d", id);
     strcpy(path, USERS_DIR);
     strcat(path, id_str);
 }
 
+static int append_to_fd(FILE* file, char* str) {
+    if (fputs(str, file) < 0) {
+        return -1;
+    }
+
+    if (fputc('\n', file) < 0) {
+        return -1;
+    }
+    return 0;
+}
 
 static int append_to_file(char *path, char *str) {
     log_debug("Appending '%s' to file '%s'", str, path);
@@ -108,14 +119,8 @@ static int append_to_file(char *path, char *str) {
         return -1;
     }
 
-    if (fputs(str, file) < 0) {
+    if (append_to_fd(file, str) < 0) {
         log_error("Error writing '%s' to file '%s'", str, path);
-        fclose(file);
-        return -1;
-    }
-
-    if (fputc('\n', file) < 0) {
-        log_error("Error writing '\\n' to file '%s'", path);
         fclose(file);
         return -1;
     }
@@ -124,7 +129,7 @@ static int append_to_file(char *path, char *str) {
     return 0;
 }
 
-int get_next_id() {
+static int get_next_id() {
     int max_id = 0;
     DIR *dir;
     struct dirent *ent;
@@ -232,8 +237,90 @@ int db_get_subscribed(char *topic, vector *subscribed) {
     return 0;
 }
 
+static int unsubscribe(int id, char *topic) {
+    log_info("Unsubscribing user %d from topic %s", id, topic);
+
+    char topic_path[strlen(TOPICS_DIR) + strlen(topic) + 1];
+    get_topic_path(topic, topic_path);
+    if (!file_exists(topic_path)) {
+        log_error("Topic file %s does not exist.", topic_path);
+        return -1;
+    }
+
+    log_debug("Opening file %s for reading.", topic_path);
+    FILE* file = fopen(topic_path, "r");
+    log_debug("Opening file %s for writing.", TMP_FILE);
+    FILE* out_file = fopen(TMP_FILE, "w+");
+
+    if (file == NULL) {
+        log_error("Error opening file %s for reading.", topic_path);
+        return -1;
+    }
+    if (out_file == NULL) {
+        log_error("Error opening file %s for reading.", TMP_FILE);
+        return -1;
+    }
+
+    char* line = NULL;
+    ssize_t read;
+    size_t len = 0;
+    while ((read = getline(&line, &len, file)) != -1) {
+        int sub_id = (int) strtol(line, (char **)NULL, 10);
+        if (sub_id != id) {
+            if (append_to_fd(out_file, line) < 0) {
+                log_error("Error appending '%s' to file '%s'", line, TMP_FILE);
+                fclose(file);
+                fclose(out_file);
+                return -1;
+            }
+        }
+    }
+    fclose(file);
+    fclose(out_file);
+    if (unlink(topic_path) < 0) {
+        log_error("Error deleting file '%s'", topic_path);
+        return -1;
+    }
+    if (!rename(TMP_FILE, topic_path)) {
+        log_error("Error renaming file '%s' to '%s'", TMP_FILE, topic_path);
+        return -1;
+    }
+
+    return 0;
+}
+
 int db_delete(int id) {
     log_info("Deleting user %d from DB.", id);
-    //TODO
+    char user_path[strlen(USERS_DIR) + INT_MAX_LENGTH];
+    get_user_path(id, user_path);
+    if (!file_exists(user_path)) {
+        log_error("User file %s does not exist.", user_path);
+        return -1;
+    }
+
+    log_debug("Opening file %s for reading.", user_path);
+    FILE* file = fopen(user_path, "r");
+    if (file == NULL) {
+        log_error("Error opening file %s for reading.", user_path);
+        return -1;
+    }
+
+    char* topic = NULL;
+    ssize_t read;
+    size_t len = 0;
+    while ((read = getline(&topic, &len, file)) != -1) {
+        unsubscribe(id, topic);
+    }
+    fclose(file);
+    if (topic) {
+        free(topic);
+    }
+
+    log_debug("Deleting file '%s'", user_path);
+    if (unlink(user_path) < 0) {
+        log_error("Error deleting file '%s'", user_path);
+        return -1;
+    }
+
     return 0;
 }
