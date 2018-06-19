a8436bbd90b4f5afbf6923ea8cc78c2b61312ecf
Gaston Snaider <gnsnaider@gmail.com>
2018-06-19 01:44:52 -0300

    User created

--- broker/db-server/db_handler.c
+++ broker/db-server/db_handler.c
@@ -10,6 +10,7 @@
 #include "../common/log/log.h"
 #include "db_message.h"
 #include "../common/ipc/socket.h"
+#include "db.h"
 
 bool graceful_quit = false;
 
@@ -32,6 +33,18 @@ void safe_exit(int client_socket, int error) {
 
 void process_create(int socket, db_msg_t *msg) {
     //TODO create in db and send response to client
+    int global_id = db_add_user();
+    if (global_id < 0) {
+        log_error("Error creating user on DB.");
+        msg->type = DB_ERROR;
+    } else {
+        log_info("User %d created on DB.", global_id);
+        msg->type = DB_OK;
+        msg->global_id = global_id;
+    }
+    if (socket_send_db(socket, msg) < 0) {
+        log_error("Error sending response to client.");
+    }
 }
 
 void process_subscribe(int socket, db_msg_t *msg) {
@@ -73,18 +86,26 @@ int main(int argc, char* argv[]) {
     sscanf(argv[1], "%d", &client_socket);
 
     db_msg_t msg;
-    log_info("Waiting for messages from client...");
-    int res = socket_receive_db(client_socket, &msg);
-    if(graceful_quit) {
-        safe_exit(client_socket, -1);
-    }
-    if (res < 0) {
-        log_error("Error reading client socket.");
-        safe_exit(client_socket, -1);
-    } else if (res == 0) {
-        log_error("Lost connection from client.");
-        safe_exit(client_socket, -1);
+    while(!graceful_quit) {
+        log_info("Waiting for messages from client...");
+        int res = socket_receive_db(client_socket, &msg);
+        if(graceful_quit) {
+            safe_exit(client_socket, -1);
+        }
+        if (res < 0) {
+            log_error("Error reading client socket.");
+            safe_exit(client_socket, -1);
+        } else if (res == 0) {
+            log_error("Lost connection from client.");
+            safe_exit(client_socket, -1);
+        }
+
+        process_message(client_socket, &msg);
     }
 
-    process_message(client_socket, &msg);
+    log_info("Closing DB handler.");
+    if (close(client_socket) < 0) {
+        log_error("Error closing DB client socket");
+    }
+
 }
