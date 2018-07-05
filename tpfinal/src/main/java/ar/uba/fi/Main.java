5e38588d1efa2045657b671b441995ce550292bd
Gaston Snaider <gnsnaider@gmail.com>
2018-07-04 23:55:11 -0300

    Starting tp final

---
+++ tpfinal/src/main/java/ar/uba/fi/Main.java
@@ -0,0 +1,11 @@
+package ar.uba.fi;
+
+import java.time.LocalTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+    }
+}
