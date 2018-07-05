c1d116d4350168b2a7d0c2871cab08c604588d69
Gaston Snaider <gnsnaider@gmail.com>
2018-07-05 13:19:44 -0300

    Starting file uploading

--- tp-final/contabilidapp/src/main/java/ar/uba/fi/contabilidapp/form/FileUploadForm.java
+++ tp-final/contabilidapp/src/main/java/ar/uba/fi/contabilidapp/form/FileUploadForm.java
@@ -1,5 +1,8 @@
 package ar.uba.fi.contabilidapp.form;
 
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -9,9 +12,13 @@ public class FileUploadForm {
 
     private String file;
 
-    public String upload() {
-        System.out.println(String.format("Received: %s", file));
-        return "/index?faces-redirect=true";
+    public void handleFileUpload(FileUploadEvent event) {
+        System.out.println("File received");
+        UploadedFile file = event.getFile();
+        byte[] contents = file.getContents();
+        String content = new String(contents);
+
+        System.out.println(String.format("Contents: %s", content));
     }
 
     public String getFile() {
