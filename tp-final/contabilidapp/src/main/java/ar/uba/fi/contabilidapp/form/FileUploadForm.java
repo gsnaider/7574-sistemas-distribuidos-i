18c5cd414b6231002f4b84527711cdc5b26a8452
Gaston Snaider <gnsnaider@gmail.com>
2018-07-05 11:29:42 -0300

    Renaming to contabilidapp

---
+++ tp-final/contabilidapp/src/main/java/ar/uba/fi/contabilidapp/form/FileUploadForm.java
@@ -0,0 +1,24 @@
+package ar.uba.fi.contabilidapp.form;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class FileUploadForm {
+
+    private String file;
+
+    public String upload() {
+        System.out.println(String.format("Received: %s", file));
+        return "index?faces-redirect=true";
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+}
